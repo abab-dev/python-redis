bf18a5924693e0c0fd3d855517086df05aaa3dc9
abab-dev <developement.ab@gmail.com>
2024-11-14 23:04:46 +0530

    multiple pings

--- app/main.py
+++ app/main.py
@@ -9,7 +9,11 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, _ = server_socket.accept() # wait for client
-    conn.sendall(b"+PONG\r\n")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
