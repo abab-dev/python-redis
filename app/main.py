9a1fcc92f12a5466619b2bbc0f1c6bdc90750324
abab-dev <developement.ab@gmail.com>
2024-11-17 11:19:41 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -74,7 +74,6 @@ async def main():
     if args.port:
         port = int(args.port)
     global datastore
-    host, port = "127.0.0.1", 6379
     kv_store = init_rdb_parser(rdb_parser_required, rdb_file_path)
     datastore |= kv_store
     server = await asyncio.start_server(handle_client, host,port)
